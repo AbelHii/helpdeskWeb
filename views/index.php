2ab60a1752f8d4aa53c5a67e1391e2e4ca9ec3f3
abel <abelhii@gmail.com>
2015-03-16 17:23:37 +0800

    added form validation

--- views/index.php
+++ views/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
+<html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
     <head>
         <meta charset="UTF-8">
         <title>Helpdesk</title>        
@@ -14,17 +14,30 @@ and open the template in the editor.
     
     <body>  
         <div id="container">
-            <img src ="../public/imgs/cmsb.jpg" height="80" width="140"/>
+            <img src ="../public/imgs/cms.jpg" alt="logo"/>
             <?php
 
                 // define variables and set to empty values
-                $username = $password = "";
-
-                if (filter_input(INPUT_SERVER, "REQUEST_METHOD") == "POST") {
-                   $username = test_input(filter_input(INPUT_POST, "username"));
-                   $password = test_input(filter_input(INPUT_POST, "password"));
+                $username = $password = $nameErr = $passErr = "";
+                
+                if ($_SERVER["REQUEST_METHOD"] == "POST"){
+                    
+                    if (empty(filter_input(INPUT_POST, "username"))) {
+                        $nameErr = "Name is required";
+                      } else {
+                        $username = test_input(filter_input(INPUT_POST, "username"));
+                        // check if name only contains letters and whitespace
+                        if (!\preg_match("/^[a-zA-Z ]*$/",$username)) {
+                            $nameErr = "Only letters and white space allowed"; 
+                        }
+                      }
+                    if (empty(filter_input(INPUT_POST, "password"))) {
+                        $passErr = "Password is required";
+                      } else {
+                        $password = test_input(filter_input(INPUT_POST, "password"));
+                      }
                 }
-
+                
                 function test_input($data) {
                    $data = trim($data);
                    $data = stripslashes($data);
@@ -35,14 +48,16 @@ and open the template in the editor.
             
             <div id="loginForm">
                 <h2>Login</h2>
-                <form id="login" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+                <form id="login" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                     <label>Name:&nbsp</label>
                     <input type="text" name="username" maxlength="20">
-                    <br><br>
+                    <span id="error"><?php echo "<br>$nameErr" ?></span>
+                    <br>
                     <label>Password: &nbsp</label>
                     <input type="password" name="password" maxlength="20">
-                    <br><br>
-                    <input type="submit" name="Login" value="Submit" style="width:100px; border-style:groove"> 
+                    <span id="error"><?php echo "<br>$passErr" ?></span>
+                    <br>
+                    <button type="submit" name="Login" value="Login" style="width:100px; border-style:groove">Login</button>
                 </form>
             </div>
             
@@ -51,6 +66,10 @@ and open the template in the editor.
                 echo "Username: " . $username;
                 echo "<br>Password: " . $password;
             ?>
+        </div>
+        
+        <div id="footer">
+            <p>Copyright 2015</p>
         </div>
     </body>
 </html>
